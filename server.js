4ddc31445ecd1aadf455d416d19629eaccd27ed3
arul-25 <arulprojectdualima@gmail.com>
2021-03-27 04:20:31 +0900

    setting router di server js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 // parse
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// pangil routes
+const router = require('./router');
+router(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
